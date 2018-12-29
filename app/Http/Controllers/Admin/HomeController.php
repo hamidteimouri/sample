3ba5400c874cca3ffa39a420674781b0fdeac344
hamidteimouri <hamidteimouri@outlook.com>
2018-12-29 22:55:48 +0330

    Add assets file , Some controller

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Exception;
 
 class HomeController extends Controller
 {
     public function index()
     {
-
+        try {
+            return view("admin.home.index", compact(""));
+        } catch (Exception $exception) {
+            dd('Error: ' . $exception->getMessage(), ' | file: ' . $exception->getFile() . ' | line: ' . $exception->getLine());
+        }
     }
 }
